eaeeac495c0758de9291fbff41f6013918b418ef
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2025-04-16 16:39:12 +0000

    a

--- pages/1_Alerts.py
+++ pages/1_Alerts.py
@@ -10,7 +10,8 @@ discord_url_2 = "https://discord.com/api/webhooks/1360719025696669756/kj7Ud2DolX
 discord_url_3 = "https://discord.com/api/webhooks/1362055824310276356/4fMbGvXG9uS-C4tF-sg4obfO-FrVhFbog0d7s6y3Dqp_yPhF8B1C8bnmA_O_EPgO372f"
 
 #rsi rascals
-discord_url_4 = "https://discord.com/api/webhooks/1362055962269323284/A1vYX-rZC2dtO2HkTM9XvlR9cc1arKYrBF7yU2mFfyYsd2U2--rIHn-KTE-A3-yPlupA"
+#discord_url_4 = "https://discord.com/api/webhooks/1362055962269323284/A1vYX-rZC2dtO2HkTM9XvlR9cc1arKYrBF7yU2mFfyYsd2U2--rIHn-KTE-A3-yPlupA"
+discord_url_4 = "https://discord.com/api/webhooks/1362104938582245386/5vzPMLTUMG8Q5X2vW6q-9X7-9ZrSNzM01Hx796ORbD4_Y4tl3NZtkI_G0jhvS9vLExhE"
 
 urls = [discord_url_1, discord_url_2, discord_url_3, discord_url_4]
 
